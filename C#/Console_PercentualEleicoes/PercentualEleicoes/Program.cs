735b582ac6e20f353e422518ebcbcbf0633cc53f
4lexander4 <oliveir4.4lexander@gmail.com>
2020-10-18 05:52:38 -0300

    Criacao de projetos Console Application

---
+++ C#/Console_PercentualEleicoes/PercentualEleicoes/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PercentualEleicoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Objetivo: Informar a % de cada tipo de voto (em branco, nulo e válido), com base nos dados solciitados ao usuário
+             * Criação: 18/10/2020
+             */
+
+            try
+            {
+                float totalEleitores, votosEmBranco, votosNulos, votosValidos;
+
+                Console.WriteLine("-------------------------\n      Bem-vindo(a)!   \n-------------------------");
+
+                Console.WriteLine("\nInforme o total de eleitores");
+                totalEleitores = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nInforme a quantidade de votos em branco");
+                votosEmBranco = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nInforme a quantidade de votos nulos");
+                votosNulos = float.Parse(Console.ReadLine());
+
+                votosValidos = totalEleitores - (votosEmBranco + votosNulos);
+
+                Console.Clear();
+
+                Console.WriteLine("-------------------------\n        Resultados   \n-------------------------");
+                Console.WriteLine("\nVotos nulos.........:" + votosNulos + " | " + ((votosNulos * 100) / totalEleitores).ToString("F") + "%");
+                Console.WriteLine("\nVotos em branco.....:" + votosEmBranco + " | " + ((votosEmBranco * 100) / totalEleitores).ToString("F") + "%");
+                Console.WriteLine("\nVotos válidos.......:" + votosValidos + " | " + ((votosValidos * 100) / totalEleitores).ToString("F") + "%");
+                Console.WriteLine("\nTotal de Eleitores..:" + totalEleitores);
+
+                Console.WriteLine("\n\n Pressione qualquer tecla para encerrar...");
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("\nInforme um valor numérico");
+                Console.WriteLine("\n\n Pressione qualquer tecla para encerrar...");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Console_ValorFinalCarro/Console_ValorFinalCarro/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_ValorFinalCarro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Objetivo: Calcular o valor final de um carro, tendo como entrada o custo de um carro.
+             * O percentual do distribuidor é 28%
+             * Impostos totais de 45%
+             * Custo de fábrica 27%
+             * 
+             * Criação: 18/10/2020
+             */
+
+            float valorTotal, custoImpostos, custoFabrica, percentualDistribuidor;
+
+            try
+            {
+                Console.WriteLine("-------------------------\n      Bem-vindo(a)!      \n-------------------------");
+                Console.WriteLine("Informe o valor total do automóvel");
+                valorTotal = float.Parse(Console.ReadLine());
+
+                custoImpostos = (valorTotal * 0.45f);
+                percentualDistribuidor = valorTotal * 0.28f;
+                custoFabrica = valorTotal * 0.27f;
+
+                Console.Clear();
+
+                Console.WriteLine("-------------------------\n      Bem-vindo(a)!      \n-------------------------");
+                Console.WriteLine("\nImpostos.....................: R$" + custoImpostos);
+                Console.WriteLine("Percentual do distribuidor...: R$" + percentualDistribuidor);
+                Console.WriteLine("Custo de fábrica.............: R$" + custoFabrica);
+                Console.WriteLine("Valor total..................: R$" + valorTotal);
+
+                Console.WriteLine("\nPressione qualquer tecla para encerrar...");
+                Console.ReadKey();
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("Informe um valor numérico");
+            }
+        }
+    }
+}
